05b977293582de10234eb5c7112d992c76fa0bdc
Cheng Fangming <chengfangming1993@gmail.com>
2015-12-21 09:40:42 +0800

    将测试视频改为网络视频

--- ch05/ch05_3/app/src/main/java/com/example/cfm/vedioview/VideoViewTest.java
+++ ch05/ch05_3/app/src/main/java/com/example/cfm/vedioview/VideoViewTest.java
@@ -1,6 +1,7 @@
 package com.example.cfm.vedioview;
 
 
+import android.net.Uri;
 import android.widget.MediaController;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,8 +24,8 @@ public class VideoViewTest extends AppCompatActivity {
 
 //        File video = new File("/mnt/sdcard/netease/cloudmusic/MV/小幸运.mp4");
 
-        //videoView.setVideoURI(Uri.parse("file:///storage/sdcard0/netease/cloudmusic/MV/xiao.mp4"));
-        videoView.setVideoPath("/storage/sdcard0/netease/cloudmusic/MV/xiao.mp4");
+        videoView.setVideoURI(Uri.parse("http://www.cfm880.com/wp-content/uploads/2015/12/videoviewdemo.mp4?_=1"));
+      //  videoView.setVideoPath("/storage/sdcard0/videoviewdemo.mp4");
         videoView.setMediaController(mediaController);
         mediaController.setMediaPlayer(videoView);
         videoView.requestFocus();
