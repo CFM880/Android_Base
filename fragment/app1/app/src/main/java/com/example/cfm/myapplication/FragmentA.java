6c6a2a1ffaf23f0d448b0796d040424a4d9dd45b
Cheng Fangming <chengfangming1993@gmail.com>
2016-01-22 21:47:27 +0800

    解决横/竖屏切换时数据消失

--- fragment/app1/app/src/main/java/com/example/cfm/myapplication/FragmentA.java
+++ fragment/app1/app/src/main/java/com/example/cfm/myapplication/FragmentA.java
@@ -16,6 +16,17 @@ public class FragmentA extends Fragment implements View.OnClickListener{
     Button button;
     int counter = 0;
     Communicator comm;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null){
+            counter = 0;
+        }else {
+            counter = savedInstanceState.getInt("counter", 0);
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +39,12 @@ public class FragmentA extends Fragment implements View.OnClickListener{
         comm = (Communicator) getActivity();
         button = (Button)getActivity().findViewById(R.id.button);
         button.setOnClickListener(this);
+    }
+    // 保存数据
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", counter);
     }
 
     @Override
--- fragment/app1/app/src/main/java/com/example/cfm/myapplication/FragmentB.java
+++ fragment/app1/app/src/main/java/com/example/cfm/myapplication/FragmentB.java
@@ -13,11 +13,22 @@ import android.widget.TextView;
  */
 public class FragmentB extends Fragment {
     TextView textView;
+    String data;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_b, container, false );
+        View view =inflater.inflate(R.layout.fragment_b, container, false );
+
+        if (savedInstanceState == null){
+
+        } else {
+            data = savedInstanceState.getString("text");
+            TextView textView = (TextView) view.findViewById(R.id.textView);
+            textView.setText(data);
+        }
+
+        return view;
     }
 
     @Override
@@ -26,7 +37,17 @@ public class FragmentB extends Fragment {
         textView = (TextView) getActivity().findViewById(R.id.textView);
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("text", data);
+
+
+    }
+
     public void changeData(String data){
+        this.data = data;
         textView.setText(data);
     }
 }
